f1d0ce335cf92708c36b7ce0dbc1c4736593664a
jravalug <jravalug@gmail.com>
2024-03-27 01:14:17 -0400

    cap 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hola amigos!");
+
+    let mut x = 5;
+    println!("El valor de x es: {}", x);
+
+    x = 3;
+    println!("El valor de x es: {}", x);
+
+    let y = 5;
+    println!("El valor de y es: {}", y);
+
+    let y = "a";
+    println!("El valor de y es: {}", y);
+    
+    const PI: f32 = 3.1416;
+    println!("El valor de PI es: {}", PI);
+    
 }
